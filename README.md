63cdbaf13e0d79afdff55f19eafc2a059153a619
Michael Stella <michael573114@gmail.com>
2011-07-15 15:27:35 -0400

    Basic library is complete and tested.

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
+# node-rrdtool
+
+A node.js implementation of Tobi Oetiker's excellent RRDTool.
+
+See http://www.mrtg.org/rrdtool/ for further information.
+
+## Status
 
 Please note that this library is currently incomplete.  Pull requests will
 be ignored until it is complete.
+
+This version aims to support RRD files created on little-endian platforms (i.e.
+PCs running Linux/BSD), either 32 or 64bit.  Only RRD version 3 and 4 files are
+supported.
+
+## Included libraries
+
+I've included the jspack library (http://code.google.com/p/jspack) in this
+library because there is not currently a package obtainable via npm.
+
+
+## Contributors
+
+This library borrows concepts and some code from javascriptRRD, which, as a
+whole, ended up being insufficient for my needs.  It's designed to run in a
+browser, and to make an AJAX request for the entire RRD, then graph it using
+Flot, whereas I was looking to make a library that reads the file directly, and
+will work with node.js.
+
+You can find this javascriptRRD library at
+http://javascriptrrd.sourceforge.net/
+
+
+## License
+
+MIT License. See LICENSE.md
--- tests/test.js
+++ tests/test.js
@@ -12,7 +12,7 @@ errors += dotest("ds_cnt", function() {
 });
 
 errors += dotest("ds_names", function() {
-    var test = rrdfile.header().getDSNames();
+    var test = rrdfile.getDSNames();
     if (test.length != 1)
         return false;
     else if (test[0] != 'cpu0')
@@ -21,18 +21,41 @@ errors += dotest("ds_names", function() {
 });
 
 errors += dotest("last update", function() {
-    return rrdfile.header().getLastUpdate() == 1310418961;
+    return rrdfile.getLastUpdate() == 1310418961;
 });
 
 errors += dotest("min step", function() {
-    return rrdfile.header().getMinStep() == 60;
+    return rrdfile.getMinStep() == 60;
 });
 
 
+errors += dotest("DS #0 name ", function() {
+    return rrdfile.getDS(0).getName() == 'cpu0';
+});
+errors += dotest("DS #0 type ", function() {
+    return rrdfile.getDS(0).getType() == 'GAUGE';
+});
 
 
+errors += dotest("RRA #0 name ", function() {
+    return rrdfile.getRRA(0).getCFName() == 'AVERAGE';
+});
+errors += dotest("RRA #0 row_count ", function() {
+    return rrdfile.getRRA(0).getRowCount() == 8928;
+});
+errors += dotest("RRA #0 ds_count ", function() {
+    return rrdfile.getRRA(0).getDSCount() == 1;
+});
+errors += dotest("RRA #0 pdp_per_row ", function() {
+    return rrdfile.getRRA(0).getPdpPerRow() == 1;
+});
 
 console.log(errors + " errors encountered.");
+
+console.log("-------------------------------------");
+console.log("DS #0 min/max: " + 
+                rrdfile.getDS(0).getMin() + " / " + 
+                rrdfile.getDS(0).getMax());
 
 function dotest(str, func) {
     var outstr = "checking " + str + "... ";
