cc9e48030d961be9e56f6b9e8af9195e734f6f99
Michael Stella <michael573114@gmail.com>
2011-07-15 17:03:12 -0400

    Changed the tests to use vows.

--- tests/test.js
+++ tests/test.js
@@ -1,72 +1,57 @@
 
+var vows = require('vows'),
+    assert = require('assert');
 var rrdtool = require('../lib/rrdtool');
 
-var rrdfile = new rrdtool.RRDFile('./tests/cputemp0.rrd');
+vows.describe('RRD tests').addBatch({
+    'RRD File Tests:': {
+        topic: new rrdtool.RRDFile('./tests/cputemp0.rrd'),
 
-///// tests /////
+        'last update': function(topic) {
+            assert.equal(topic.getLastUpdate(), 1310418961);
+        },
+        'min step': function(topic) {
+            assert.equal(topic.getMinStep(), 60);
+        },
 
-var errors = 0;
+        'DS:': {
+            'count': function(topic) {
+                assert.equal(topic.header().ds_cnt, 1);
+            },
+            'name count': function(topic) {
+                assert.equal(topic.getDSNames().length, 1);
+            },
+            'name match': function(topic) {
+                assert.equal(topic.getDSNames()[0], 'cpu0');
+            },
+            'DS #0:': {
+                'name': function(topic) {
+                    assert.equal(topic.getDS(0).getName(), 'cpu0');
+                },
+                'type': function(topic) {
+                    assert.equal(topic.getDS(0).getType(), 'GAUGE');
+                }
+            }
+        },
+        'RRA:': {
+            'count': function(topic) {
+                assert.equal(topic.getRRACount(), 10);
+            },
 
-errors += dotest("ds_cnt", function() {
-    return rrdfile.header().ds_cnt == 1;
-});
-
-errors += dotest("ds_names", function() {
-    var test = rrdfile.getDSNames();
-    if (test.length != 1)
-        return false;
-    else if (test[0] != 'cpu0')
-        return false;
-    return true;
-});
-
-errors += dotest("last update", function() {
-    return rrdfile.getLastUpdate() == 1310418961;
-});
-
-errors += dotest("min step", function() {
-    return rrdfile.getMinStep() == 60;
-});
-
-
-errors += dotest("DS #0 name ", function() {
-    return rrdfile.getDS(0).getName() == 'cpu0';
-});
-errors += dotest("DS #0 type ", function() {
-    return rrdfile.getDS(0).getType() == 'GAUGE';
-});
-
-
-errors += dotest("RRA #0 name ", function() {
-    return rrdfile.getRRA(0).getCFName() == 'AVERAGE';
-});
-errors += dotest("RRA #0 row_count ", function() {
-    return rrdfile.getRRA(0).getRowCount() == 8928;
-});
-errors += dotest("RRA #0 ds_count ", function() {
-    return rrdfile.getRRA(0).getDSCount() == 1;
-});
-errors += dotest("RRA #0 pdp_per_row ", function() {
-    return rrdfile.getRRA(0).getPdpPerRow() == 1;
-});
-
-console.log(errors + " errors encountered.");
-
-console.log("-------------------------------------");
-console.log("DS #0 min/max: " + 
-                rrdfile.getDS(0).getMin() + " / " + 
-                rrdfile.getDS(0).getMax());
-
-function dotest(str, func) {
-    var outstr = "checking " + str + "... ";
-    if (func()) {
-        console.log(outstr + 'OK');
-        return 0;
+            'RRA #0:': {
+                'name': function(topic) {
+                    assert.equal(topic.getRRA(0).getCFName(), 'AVERAGE');
+                },
+                'row_count': function(topic) {
+                    assert.equal(topic.getRRA(0).getRowCount(), 8928);
+                },
+                'ds_count': function(topic) {
+                    assert.equal(topic.getRRA(0).getDSCount(), 1);
+                },
+                'pdp_per_row': function(topic) {
+                    assert.equal(topic.getRRA(0).getPdpPerRow(), 1);
+                }
+            }
+        }
     }
-    else {
-        console.log(outstr + 'FAIL');
-        return 1;
-    }
-}
-
-
+}).export(module);
